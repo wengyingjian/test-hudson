acdf3a83361bc297b4f7dfe759342af576355cb7
wengyingjian <q291611265@gmail.com>
2015-12-26 16:12:10 +0800

    log

--- src/main/java/com/asd/hudson/controller/HelloController.java
+++ src/main/java/com/asd/hudson/controller/HelloController.java
@@ -3,6 +3,8 @@
  */
 package com.asd.hudson.controller;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -16,9 +18,12 @@ import org.springframework.web.bind.annotation.ResponseBody;
 @Controller
 public class HelloController {
 
+    private static final Logger logger = LoggerFactory.getLogger(HelloController.class);
+
     @RequestMapping("hello")
     @ResponseBody
     public String hello(String name) {
+        logger.info("request for /HelloController...");
         return "hello:" + name;
     }
 }
